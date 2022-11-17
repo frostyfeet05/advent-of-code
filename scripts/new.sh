3ee1bcd69719e6496e564d0283258a14a7df7358
Sander Goyvaerts <sander.goyvaerts@gmail.com>
2022-11-17 18:33:20 +0100

    new create script

--- scripts/new.sh
+++ scripts/new.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-mkdir -p "year/$1/day/$2/puzzle/$3/"
 
-if [ -f "year/$1/day/$2/puzzle/$3/index.ts" ]; then
-  echo "Puzzle already exists, I won't overwrite"
+if [ -f "year/$1/day/$2/puzzle/1/index.ts" -o -f "year/$1/day/$2/puzzle/2/index.ts" -o -f "year/$1/day/$2/puzzle/shared/index.ts" ]; then
+  echo "Puzzles already exists, I won't overwrite"
 else
-  cp "scripts/template.ts" "year/$1/day/$2/puzzle/$3/index.ts"
+  echo "Making folders and files for year $1 day $2..."
+
+  mkdir -p "year/$1/day/$2/puzzle/1/"
+  mkdir -p "year/$1/day/$2/puzzle/2/"
+  mkdir -p "year/$1/day/$2/puzzle/shared/"
+
+  cp "scripts/template-puzzle.txt" "year/$1/day/$2/puzzle/1/index.ts"
+  cp "scripts/template-puzzle.txt" "year/$1/day/$2/puzzle/2/index.ts"
+  cp "scripts/template-shared.txt" "year/$1/day/$2/puzzle/shared/index.ts"
+
+  echo "Done! Good luck!"
+  echo
 fi
