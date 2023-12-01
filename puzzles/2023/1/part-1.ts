1ee737c3e807bfe6068fd561b92ee2af6b26eb4c
Sander Goyvaerts <sander.goyvaerts@gmail.com>
2023-12-01 10:19:14 +0100

    added 2023 day 1

---
+++ puzzles/2023/1/part-1.ts
@@ -0,0 +1,22 @@
+import {Puzzle, input} from './shared';
+
+const solve = (input: string): number => {
+    const lines = Puzzle.parse(input);
+    const numbersOnly = lines
+        .map(line => line
+            .split('')
+            .filter((x) => /[^a-z]$/.test(x))
+            .join(''))
+        .map(line => {
+            const first = line[0];
+            const last = line[line.length - 1];
+            return parseInt(`${first}${last}`);
+        });
+
+    return numbersOnly.reduce((a, b) => a + b);
+};
+
+const result = solve(input);
+console.log(`Result is ${result}`);
+
+export {};
---
+++ puzzles/2023/1/shared.ts
@@ -0,0 +1,5 @@
+export const input = ``;
+
+export const Puzzle = {
+  parse: (input: string): string[] => input.split("\n")
+};
